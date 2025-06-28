07ddf1311a427ba3c3eb00edfe5bc3a84c745bf6
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-06-28 16:31:53 -0500

    fix: refactor create_task method to ensure task initialization

--- tasks/application/services.py
+++ tasks/application/services.py
@@ -10,4 +10,17 @@ class TaskService:
         return self.repository.list_task()
     
     def create_task(self, task: Task):
+        task = Task(id=None, **task)
         return self.repository.create_task(task)
+    
+    def get_task(self, task_id: int):
+        return self.repository.get_task(task_id)
+    
+    def update_task(self, task_id: int, task: Task):
+        task = Task(id=task_id, **task)
+        return self.repository.update_task(task_id, task)
+    
+    def delete_task(self, task_id: int):
+        return self.repository.delete_task(task_id)
+    
+    
