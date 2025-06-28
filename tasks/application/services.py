58d5f71858eaf66bb9614c59e185e25a311821cf
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-06-28 14:10:35 -0500

    add TaskService class for task management operations

---
+++ tasks/application/services.py
@@ -0,0 +1,13 @@
+from domian.ports import TaskRepositoryPort
+from domian.entity import Task
+
+class TaskService:
+    
+    def __init__(self, repository: TaskRepositoryPort):
+        self.repository = repository
+    
+    def list_task(self):
+        return self.repository.list_task()
+    
+    def create_task(self, task: Task):
+        return self.repository.create_task(task)
