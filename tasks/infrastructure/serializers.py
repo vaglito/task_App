6d8341bf9d845a7d5c605f8a9c16f5a40623341b
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-06-28 15:48:26 -0500

    remove TaskSerializer as part of task management refactor

--- tasks/infrastructure/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import TaskModel
-
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TaskModel
-        fields = '__all__'
