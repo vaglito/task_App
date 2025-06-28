4f841c84a62d43b6a8cb6442f8e662e64d65ff0b
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-06-28 16:41:23 -0500

    fix: add CRUD methods to DjangoTaskRepository for task management

--- tasks/infrastructure/repositories.py
+++ tasks/infrastructure/repositories.py
@@ -14,3 +14,25 @@ class DjangoTaskRepository(TaskRepositoryPort):
             state=task.state
         )
         return Task(**task_obj.__dict__)
+    
+    def getTaskById(self, task_id: int):
+        return self._to_entity(TaskModel.objects.get(id=task_id))
+    
+    def updateTask(self, task_id: int, task: Task):
+        task_obj = TaskModel.objects.get(id=task_id)
+        task_obj.title = task.title
+        task_obj.description = task.description
+        task_obj.state = task.state
+        task.obj.save()
+        return self._to_entity(task_obj)
+    
+    def deleteTask(self, task_id: int):
+        return TaskModel.objects.get(id=task_id).delete()
+    
+    def _to_entity(self, model: TaskModel):
+        return Task(
+            id=model.id,
+            title=model.title,
+            description=model.description,
+            state=model.state,
+        )
