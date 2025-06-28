e1321068747492afb3dd8ee8b39420322d02ec78
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-06-28 15:16:55 -0500

    add initial migration and models for task management

---
+++ tasks/infrastructure/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class TaskModel(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    state = models.BooleanField(default=False)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+        ordering = ['title']
+    
+    def __str__(self):
+        return self.title
