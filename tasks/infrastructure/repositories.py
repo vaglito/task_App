38df7d1b507829748cd64069ac7ce30ef243af91
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-06-28 15:16:42 -0500

    add DjangoTaskRepository implementation for task management

---
+++ tasks/infrastructure/repositories.py
@@ -0,0 +1,16 @@
+from domian.entity import Task
+from domian.ports import TaskRepositoryPort
+from .models import TaskModel
+
+class DjangoTaskRepository(TaskRepositoryPort):
+
+    def list_task(self):
+        return [Task(**task.__dic__) for task in TaskModel.objects.all()]
+    
+    def create_task(self, task: Task):
+        task_obj = TaskModel.objects.create(
+            title=task.title,
+            description=task.description,
+            state=task.state
+        )
+        return Task(**task_obj.__dic__)
