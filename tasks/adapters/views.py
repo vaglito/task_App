21f976e500f230ad479ed6838327b235d7e0d33a
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-02 03:17:43 -0500

    fix: refactor destroy method in TaskViewSet to retrieve task before deletion

--- tasks/adapters/views.py
+++ tasks/adapters/views.py
@@ -8,23 +8,24 @@ from .serializers import TaskSerializer
 repo = DjangoTaskRepository()
 service = TaskService(repo)
 
+
 class TaskViewSet(ViewSet):
 
     def list(self, request):
         tasks = service.list_task()
         serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
+
     def create(self, request):
         serializer = TaskSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         task = service.create_task(serializer.validated_data)
         return Response(TaskSerializer(task).data, status=status.HTTP_201_CREATED)
-    
+
     def retrieve(self, request, pk=None):
         task = service.get_task(int(pk))
         return Response(TaskSerializer(task).data, status=status.HTTP_200_OK)
-    
+
     def partial_update(self, request, pk=None):
         task = service.get_task(int(pk))
         data = request.data
@@ -42,7 +43,8 @@ class TaskViewSet(ViewSet):
 
         update_task = service.update_task(int(pk), serializer.validated_data)
         return Response(TaskSerializer(update_task).data, status=status.HTTP_200_OK)
-    
+
     def destroy(self, request, pk=None):
-        service.delete_task(int(pk))
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        task = service.get_task(int(pk))
+        service.delete_task(task.id)
+        return Response({"detail": "Tarea correctamente eliminada."}, status=status.HTTP_204_NO_CONTENT)
