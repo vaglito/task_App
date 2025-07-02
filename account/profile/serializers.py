c5dea015abbd730e1d8b2bde60ff65196d9f97dc
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-02 12:20:50 -0500

    feat: add extra_kwargs to AccountSerializer for required fields

--- account/profile/serializers.py
+++ account/profile/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from .models import AccountModel
 
 class AccountSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = AccountModel
         fields = '__all__'
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'year_of_birth': {'required': True},
+        }
