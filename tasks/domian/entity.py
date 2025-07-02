9b9b405d6eda9c091dca51b0b56416bb8f7c67ff
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-02 12:50:26 -0500

    feat: add user field to Task and TaskModel for user association

--- tasks/domian/entity.py
+++ tasks/domian/entity.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass, field
 from datetime import datetime
+from account.domain.entity import Account
 
 
 @dataclass
 class Task:
     id: int
     title: str
+    user: Account
     is_active: bool = True
     description: str = ""
     state: bool = False
--- tasks/infrastructure/models.py
+++ tasks/infrastructure/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from account.profile.models import AccountModel
+
 
 class TaskModel(models.Model):
     title = models.CharField(max_length=255)
     is_active = models.BooleanField(null=True, default=True)
+    user = models.ForeignKey(
+        AccountModel,
+        on_delete=models.CASCADE,
+        related_name='users'
+    )
     description = models.TextField(blank=True)
     state = models.BooleanField(default=False)
     updated_at = models.DateTimeField(auto_now=True)
