8e7ee2286f9e1a7ba373c05b623fb205d8721fef
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-02 09:16:37 -0500

    fix: add account.profile to INSTALLED_APPS and set custom AUTH_USER_MODEL

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'tasks.infrastructure'
+    'tasks.infrastructure',
+    'account.profile',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'profile.AccountModel'
