3170ab6aa9a28c511239b10afebeb2e3f2a879de
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-02 12:50:32 -0500

    feat: add user field to TaskSerializer for user association

--- tasks/adapters/serializers.py
+++ tasks/adapters/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from ..infrastructure.models import TaskModel
+from account.profile.serializers import AccountSerializer
 
 class TaskSerializer(serializers.ModelSerializer):
+    user = AccountSerializer()
     class Meta:
         model = TaskModel
         fields = '__all__'
