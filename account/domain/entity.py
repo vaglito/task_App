a123a35e5122c39ddbe73929f36d00733dd1aa13
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-02 12:20:55 -0500

    feat: reorder fields in Account class for clarity and consistency

--- account/domain/entity.py
+++ account/domain/entity.py
@@ -4,12 +4,13 @@ from datetime import datetime, date
 @dataclass
 class Account:
     id: int
-    is_active: bool = True
-    name: str
+    first_name: str
     last_name: str
+    username: str
     email: str
     password: str
     year_of_birth: date
+    is_active: bool = True
     updated_at: datetime = field(default_factory=datetime.now)
     created_at: datetime = field(default_factory=datetime.now)
 
