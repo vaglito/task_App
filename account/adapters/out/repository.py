3e873a2e838ae2149b114308b8213e8f645f22d5
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-02 12:21:00 -0500

    refactor: standardize method naming and improve account creation logic

--- account/adapters/out/repository.py
+++ account/adapters/out/repository.py
@@ -5,18 +5,22 @@ from ...profile.models import AccountModel
 
 class AccountRepository(AccountRepositoryPort):
 
-    def get_account(self, account_id: int, account: Account):
+    def getAccount(self, account_id: int):
         account = get_object_or_404(AccountModel.objects.filter(is_active=True), id=account_id)
 
         return account
     
-    def create_account(self, account: Account):
-        account_obj = AccountModel.objects.create(
-            name=account.name,
+    def createAccount(self, account: Account):
+        account_obj = AccountModel(
+            first_name=account.first_name,
             last_name=account.last_name,
+            username=account.username,
             email=account.email,
             password=account.password,
             year_of_birth=account.year_of_birth
         )
+
+        account_obj.set_password(account.password)
+        account_obj.save()
 
         return account_obj
